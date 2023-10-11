da4eeac0298cd84e0f72cde3a2e2d27947bea40b
Diego <Xerlocs@yahoo.com>
2023-10-11 14:30:08 -0300

    Initial commit

---
+++ app/src/main/java/cl/dv/listacompras/ListaCompra.kt
@@ -0,0 +1,18 @@
+package cl.dv.listacompras
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ListaCompra : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_compra)
+
+    }
+}
+
+//Listar las cosas a comprar
+
+//Boton para agregar cosas a comprar
